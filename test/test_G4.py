13022863fbcce4a0793f63ab05a1df563b4bc94b
Sam Windels <sam.windels@gmail.com>
2022-10-14 14:00:14 +0200

    testing

--- test/test_G4.py
+++ test/test_G4.py
@@ -12,206 +12,216 @@ import pandas as pd
 
 
 def contains_graphlet(A, graphlet):
-    n = A.shape[0]
+    n = A.shape[0] 
+    e = int((n * (n-1)) / 2)
     triu = gradco.count(A, n, graphlet)
     AG = squareform(triu)
-    print(A)
-    print(AG)
-    return np.array_equal(np.ones((n)), triu)
+    print("A\n", A)
+    print("AG", np.array_equal(np.ones((e)), triu), "\n", AG)
+    return np.array_equal(np.ones((e)), triu)
+
+def not_contains_graphlet(A, graphlet):
+    n = A.shape[0] 
+    e = int((n * (n-1)) / 2)
+    triu = gradco.count(A, n, graphlet)
+    AG = squareform(triu)
+    print("A\n", A)
+    print("AG", np.array_equal(np.zeros((e)), triu), "\n", AG)
+    return np.array_equal(np.zeros((e)), triu)
 
 def test_graphlet_4():
 
     graphlet = 4
 
     A = squareform([0, 0, 0, 0, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 0, 0, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 0, 0, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 0, 0, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 1, 0, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 1, 0, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 1, 0, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 1, 0, 0, 0])
     assert contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 0, 1, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 0, 1, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 0, 1, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 0, 1, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 1, 1, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 1, 1, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 1, 1, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 1, 1, 0, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 0, 0, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 0, 0, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 0, 0, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 0, 0, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 1, 0, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 1, 0, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 1, 0, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 1, 0, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 0, 1, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 0, 1, 1, 0])
     assert contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 0, 1, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 0, 1, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 1, 1, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 1, 1, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 1, 1, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 1, 1, 1, 0])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 0, 0, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 0, 0, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 0, 0, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 0, 0, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 1, 0, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 1, 0, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 1, 0, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 1, 0, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 0, 1, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 0, 1, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 0, 1, 0, 1])
     assert contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 0, 1, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 1, 1, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 1, 1, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 1, 1, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 1, 1, 0, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 0, 0, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 0, 0, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 0, 0, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 0, 0, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 1, 0, 1, 1])
     assert contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 1, 0, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 1, 0, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 1, 0, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 0, 1, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 0, 1, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 0, 1, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 0, 1, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 0, 1, 1, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 0, 1, 1, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([0, 1, 1, 1, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
     A = squareform([1, 1, 1, 1, 1, 1])
-    assert not contains_graphlet(A, graphlet)
+    assert not_contains_graphlet(A, graphlet)
 
