f7d2929e7a820709740363a9b0e965eddacfcbc4
swindels <sam.windels@bsc.es>
2023-09-06 18:25:54 +0200

    eigencentrality

--- gradco_python_interface.py
+++ gradco_python_interface.py
@@ -5,6 +5,7 @@ import networkx as nx
 import subprocess
 import os
 from scipy.spatial.distance import squareform
+from scipy.linalg import eigh
 import pandas as pd
 import gradco
 from itertools import combinations
@@ -179,26 +180,142 @@ def count(G, adj_type):
         case _:
             ValueError('invalid adjacency type')
 
+def normalize_symmetric(A):
+    """
+        Divide each entry of an adjacency matrix by square root of the
+        rowsum and colsum. Implementation assumes M is symmetric.
+    """
+    D_array=np.sum(A, axis=1)
+    D_array=np.power(D_array,0.5) + np.finfo(float).eps
+    A = A / D_array[:,None]
+    A = A / D_array[None,:]
+    return A
+
+def power_iteration(matrix, num_iterations, convergence_threshold=1e-6):
+    # Generate a random initial guess for the dominant eigenvector
+    n = matrix.shape[0]
+    eigen_vector = np.random.rand(n)
+
+    for iteration in range(num_iterations):
+        # Compute the matrix-vector product
+        matrix_times_vector = np.dot(matrix, eigen_vector)
+
+        # Normalize the result to prevent divergence
+        eigen_vector = matrix_times_vector / np.linalg.norm(matrix_times_vector)
+
+        # Compute the eigenvalue estimate
+        eigen_value = np.dot(np.dot(eigen_vector, matrix), eigen_vector)
+
+        # Check for convergence
+        if iteration > 0:
+            eigen_value_change = abs(eigen_value - prev_eigen_value)
+            if eigen_value_change < convergence_threshold:
+                print(f"Poweriteration converged after {iteration} iterations.")
+                break
+
+        prev_eigen_value = eigen_value
+
+    return eigen_value, eigen_vector
+
+def compute_eigencentrality(A):
+
+    # check matrix is square
+    assert A.shape[0] == A.shape[1]
+    A = normalize_symmetric(A)
+    n = A.shape[0]
+    
+    # get the dominant eigen vector with scipy
+    # _, eigenvector = eigh(A, subset_by_index=(n-1,n-1))  
+    
+    # get the dominant eigen vector with numpy
+    # _, eigenvectors = np.linalg.eig(A)
+    # eigenvector = eigenvectors[:, n-1]
+    
+    # get the dominant eigen vector via power iteration
+    _, eigenvector = power_iteration(A, 300, 1e-5)
+
+    # eigenvector could be rotated, make sure it is non-negative
+    if np.sum(eigenvector<=0.0) == n:
+        eigenvector = -eigenvector 
+    # check centralities are non-negative
+    assert np.sum(eigenvector<0.0) == 0
+    return eigenvector
+
+def compute_graphlet_eigencentralities(G):
+
+    if G.number_of_edges() == 0:
+        raise ValueError("Graph can't be empty")
+
+    eigencentralities = []
+    A = nx.to_numpy_array(G, dtype=int)
+    # making sure the matrix is undirected
+    A = A+A.transpose()
+    A[A>0]=1
+
+    # standard symmetrically normalized eigencentrality (e.g., graphlet G0)
+    eigencentralities.append(compute_eigencentrality(A))
+
+    rows, cols, n, order = format_gradco_input(G)
+    As_sparse = gradco.count(rows, cols, n)  
+
+    # graphlet eigencentralities 
+    print(1)
+    A = compute_graphlet_adjacency_1(As_sparse, n, order)
+    eigencentralities.append(compute_eigencentrality(A))
+    
+    print(2)
+    A = compute_graphlet_adjacency_2(As_sparse, n, order)
+    eigencentralities.append(compute_eigencentrality(A))
+    
+    print(3)
+    A = compute_graphlet_adjacency_3(As_sparse, n, order)
+    eigencentralities.append(compute_eigencentrality(A))
+    
+    print(4)
+    A = compute_graphlet_adjacency_4(As_sparse, n, order)
+    eigencentralities.append(compute_eigencentrality(A))
+    
+    print(5)
+    A = compute_graphlet_adjacency_5(As_sparse, n, order)
+    eigencentralities.append(compute_eigencentrality(A))
+            
+    print(6)
+    A = compute_graphlet_adjacency_6(As_sparse, n, order)
+    eigencentralities.append(compute_eigencentrality(A))
+    
+    print(7)
+    A = compute_graphlet_adjacency_7(As_sparse, n, order)
+    eigencentralities.append(compute_eigencentrality(A))
+            
+    print(8)
+    A = compute_graphlet_adjacency_8(As_sparse, n, order)
+    eigencentralities.append(compute_eigencentrality(A))
+
+    return eigencentralities
 
 
 def main():
 
    
     G = nx.scale_free_graph(5000)
-    rows, cols, n, order = format_gradco_input(G)
-    if len(rows) == 0:
-        # input graph is empty, edge case not yet taken care of properly
-        return np.zeros((n, n))
-    
-    #As_sparse is a list of all the orbit adjacency matrix in sparse matrix form
-    As_sparse = gradco.count(rows, cols, n)  
-    
-    # example orbit adjacency matrix (dense numpy 2d array)
-    # not to be used in graph fusion, or anywhere else for that matter, until published
-    A1_2 = format_gradco_output(As_sparse, 'A1_2', n, order)
 
-    # example graphlet adjacency matrix (dense numpy 2d array)
-    AG1 = compute_graphlet_adjacency_1(As_sparse, n, order)
+    centralities = compute_graphlet_eigencentralities(G)
+    return 
+
+    #rows, cols, n, order = format_gradco_input(G)
+    #if len(rows) == 0:
+    #    # input graph is empty, edge case not yet taken care of properly
+    #    return np.zeros((n, n))
+    
+    ##As_sparse is a list of all the orbit adjacency matrix in sparse matrix form
+    #As_sparse = gradco.count(rows, cols, n)  
+    
+    ## example orbit adjacency matrix (dense numpy 2d array)
+    ## not to be used in graph fusion, or anywhere else for that matter, until published
+    #A1_2 = format_gradco_output(As_sparse, 'A1_2', n, order)
+
+    ## example graphlet adjacency matrix (dense numpy 2d array)
+    #AG1 = compute_graphlet_adjacency_1(As_sparse, n, order)
 
 
 if __name__ == "__main__":
