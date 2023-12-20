3d4d812315999c4496856ccd9a8bc42799236015
swindels <sam.windels@bsc.es>
2023-12-20 15:33:32 +0100

    REMOVED REDUNDANT COMMENTS IN SETUP.PY

--- setup.py
+++ setup.py
@@ -6,26 +6,6 @@ import numpy as np
 
 def main():
 
-    _DEBUG = False
-    # Generally I write code so that if DEBUG is defined as 0 then all optimisations
-    # # are off and asserts are enabled. Typically run times of these builds are x2 to x10
-    # # release builds.
-    # # If DEBUG > 0 then extra code paths are introduced such as checking the integrity of
-    # # internal data structures. In this case the performance is by no means comparable
-    # # with release builds.
-    # _DEBUG_LEVEL = 0
-    #
-    # # Common flags for both release and debug builds.
-    # extra_compile_args = sysconfig.get_config_var('CFLAGS').split()
-    # extra_compile_args += ["-Wall", "-Wextra"]
-    # if _DEBUG:
-    #     extra_compile_args += ["-g3", "-O0",
-    #                            "-DDEBUG=%s" % _DEBUG_LEVEL, "-UNDEBUG"]
-    # else:
-    #     # careful, does this compile for current system architecture only?
-    #     extra_compile_args += ["-DNDEBUG", "-O2"]
-
-    # print(extra_compile_args)
     
     c_module = Extension("gradco_c_routines",
                               sources=["c_module/directed_graph.cpp", 
@@ -36,21 +16,20 @@ def main():
                                        "c_module/directed_graph.hh"],
                               include_dirs=[ np.get_include(), "."],
                               language='c++',
-                              extra_compile_args=['-std=c++17', "-O2"]
+                              extra_compile_args=['-std=c++20', "-O2"]
                               )
-
     # c++20 
     # gnu++20 
     # c++2b => 23 working draft 
     # gnu++2b => working draft with GNU extensions 
 
-    setup(name="gradco",  # Name of the package. Used e.g. by pip to install the package.
+    setup(name="gradco",  # Name of the package. Used at install: pip install gradco
           version="0.0.2",
-          description="Orbit adjacency counter.",
+          description="GRaphlet and Orbit Adjacency Counter (GROADCO).",
           author="Sam Windels",
           author_email="sam.windels@gmail.com",
           ext_modules=[c_module],
-          py_modules=["gradco"],  # Refers to gradco.py. Name of the module.
+          py_modules=["gradco"],  # Refers to gradco.py. Name of the module. Used as: import gradco
           )
 
 
