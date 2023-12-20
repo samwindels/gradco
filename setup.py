8af05f0cc3dcead718c1cff5aa555eae69e402a5
swindels <sam.windels@bsc.es>
2023-12-20 15:31:21 +0100

    REMOVED REDUNDANT COMMENTS IN SETUP.PY

--- setup.py
+++ setup.py
@@ -28,34 +28,15 @@ def main():
     # print(extra_compile_args)
     
     c_module = Extension("gradco_c_routines",
-                              # sources=["src/gradco_pkg/c_subpkg/directed_graph.cpp", 
-                              #          "src/gradco_pkg/c_subpkg/gradco_module.cpp",
-                              #          "src/gradco_pkg/c_subpkg/matrix.cpp"],
-                              # depends=["src/gradco_pkg/c_subpkg/unordered_dense.h", 
-                              #          'src/gradco_pkg/c_subpkg/matrix.hh', 
-                              #          "src/gradco_pkg/c_subpkg/directed_graph.hh"],
                               sources=["c_module/directed_graph.cpp", 
                                        "c_module/gradco_module.cpp",
                                        "c_module/matrix.cpp"],
                               depends=["c_module/unordered_dense.h", 
                                        'c_module/matrix.hh', 
                                        "c_module/directed_graph.hh"],
-                              # headers=["unordered_dense.h", 'matrix.hh'],
-                              # library_dirs=['/opt/local/lib/boost'],
-                              # library_dirs=['/opt/local/libexec/boost/1.76/lib/'],
-                              # runtime_library_dirs=['/opt/local/libexec/boost/1.76/lib/'],
-                              # runtime_library_dirs=['/opt/local/lib/boost'],
-                              # libraries=['boost_python'],
-                              # include_dirs=['/opt/local/include', np.get_include()],
-                              # include_dirs=['/opt/local/include/boost/', np.get_include()],
                               include_dirs=[ np.get_include(), "."],
                               language='c++',
                               extra_compile_args=['-std=c++17', "-O2"]
-                              # extra_compile_args=['-std=c++17', "-O2", "-march=native"]
-                              # extra_compile_args=["-O2", "-march=native"]
-                              # extra_compile_args=['-std=c++20', "-O3", "-march=native"]
-                              # extra_compile_args=['-std=c++20', '-g', '-O0']
-                              # extra_compile_args=['-std=c++20', "-O2"]
                               )
 
     # c++20 
@@ -63,24 +44,13 @@ def main():
     # c++2b => 23 working draft 
     # gnu++2b => working draft with GNU extensions 
 
-
     setup(name="gradco",  # Name of the package. Used e.g. by pip to install the package.
           version="0.0.2",
           description="Orbit adjacency counter.",
           author="Sam Windels",
           author_email="sam.windels@gmail.com",
-          # setup_requires=["numpy"],  # Just numpy here
-          # install_requires=["numpy"],  # Add any of your other dependencies here
-          # py_modules=["src/gradco_pkg/gradco"],
           ext_modules=[c_module],
-          # package_dir={"": "src/gradco_pkg"},
-          # packages=find_packages()
-          # py_modules=["gradco_pkg.gradco"],
-          # packages = ["gradco_pkg"],
-          # package_dir={"gradco_pkg": "src/gradco_pkg"},
           py_modules=["gradco"],  # Refers to gradco.py. Name of the module.
-          # packages = ["gradco_pkg"],
-          # package_dir={"gradco_pkg": "."},
           )
 
 
