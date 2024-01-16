197aba5c2e2f85e2fd06defbf29e46040dca92a7
swindels <sam.windels@bsc.es>
2024-01-16 17:07:29 +0100

    sdm constructor

--- c_module/symmetric_dense_matrix.hh
+++ c_module/symmetric_dense_matrix.hh
@@ -10,14 +10,41 @@ class SymmetricDenseMatrix{
 
 	private:
 		int n; // Dimension of matrix.
+		/* offsets array to enable fast indexing from row col format to flat array */
+		int *offsets;
+		unsigned int* array;
+
+
+	int to_flat_index(i, j){
+		if (i<j){ 
+			return offsets[i]+j;
+		}
+		else{ 
+			return offsets[j]+i; 
+		}
+	}
 	
 	public:
 		SymmetricDenseMatrix(int n){
+			/* init offsets array. */
+			offsets = malloc((n-1) * sizeof(*offsets)); /* Memory allocation */
+			if (!offsets) {  
+    			   printf( "memory allocation 'offsets' failed\n" );
+    			   exit(-1);
+			}
+			int incumbent = -n;
+			for(int i=0; i<(n-1); i++){
+				offsets[i] = incumbent + n-i -1; 
+				incumbent = offsets[i];
+			}
+			
+		        array = (unsigned **)malloc(n*(n-1)/2 sizeof(int *));
 		};
-
+	
+	void add_scalar(int i, j, v);
 	void increment_all_2_all(int a, int b);
-		void increment_from_to(int a, int b);
-		PyObject* to_numpy();
+	void increment_from_to(int a, int b);
+	PyObject* to_numpy();
 
 
 };
