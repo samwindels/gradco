72e52f91b186da5723505803408702a2462c1ef4
swindels <sam.windels@bsc.es>
2024-01-16 17:36:44 +0100

    sdm moved constructor

--- c_module/symmetric_dense_matrix.cpp
+++ c_module/symmetric_dense_matrix.cpp
@@ -1,3 +1,38 @@
+#include "symmetric_dense_matrix.hh"
 
+
+SymmetricDenseMatrix::SymmetricDenseMatrix(int n) : n(n){
+			
+		/* init offsets array. */
+		this-> offsets = (unsigned int*) malloc((n-1) * sizeof(*offsets)); /* Memory allocation */
+		if (!offsets) {  
+    			printf( "memory allocation 'offsets' failed\n" );
+    			exit(-1);
+		}
+		int incumbent = -n;
+		for(int i=0; i<(n-1); i++){
+			offsets[i] = incumbent + n-i -1; 
+			incumbent = offsets[i];
+		}
+		
+		this->len = this->n*(this->n-1)/2;
+		this->array = (unsigned int *) malloc(this->len * sizeof(int *));
+		for (int i=0; i<this->len; i++){
+			this->array[i] = 0;
+		}
+}
+
+void SymmetricDenseMatrix::add_scalar(int i, int j, int v){
+	this->array[to_flat_index(i,j)] += v;
+}
+
+int SymmetricDenseMatrix::to_flat_index(int i, int j){
+	if (i<j){ 
+		return offsets[i]+j;
+	}
+	else{ 
+		return offsets[j]+i; 
+	}
+}
 
 
--- c_module/symmetric_dense_matrix.hh
+++ c_module/symmetric_dense_matrix.hh
@@ -18,34 +18,11 @@ class SymmetricDenseMatrix{
 		int to_flat_index(int i, int j);
 	
 	public:
-		SymmetricDenseMatrix(int n){
-			this->n = n;
-			
-			/* init offsets array. */
-			this-> offsets = (unsigned int*) malloc((n-1) * sizeof(*offsets)); /* Memory allocation */
-			if (!offsets) {  
-    			   printf( "memory allocation 'offsets' failed\n" );
-    			   exit(-1);
-			}
-			int incumbent = -n;
-			for(int i=0; i<(n-1); i++){
-				offsets[i] = incumbent + n-i -1; 
-				incumbent = offsets[i];
-			}
-		
-			this->len = this->n*(this->n-1)/2;
-		        this->array = (unsigned int *) malloc(this->len * sizeof(int *));
-			for (int i=0; i<this->len; i++){
-				this->array[i] = 0;
-			}
-		};
+		SymmetricDenseMatrix(int n);
+		void add_scalar(int i, int j, int v);
+		void increment_all_2_all(int a, int b);
+		void increment_from_to(int a, int b);
+		PyObject* to_numpy();
+	};
 	
-	void add_scalar(int i, int j, int v);
-	void increment_all_2_all(int a, int b);
-	void increment_from_to(int a, int b);
-	PyObject* to_numpy();
-
-
-};
-
 #endif // SYMMETRIC_DENSE_MATRIX_H
